ff649bc30e2b9d426cc18a5230d99a2ccfc906a4
Kiritee <kiritee.amruth@innovaccer.com>
2017-01-31 02:13:22 +0530

    Added blackbox remover, rgb2gray and gray2bw functions

--- preprocessing.py
+++ preprocessing.py
@@ -1,55 +1,54 @@
-import numpy as np
 from PIL import Image, ImageChops
+from utilities import *
+import numpy as np
+import cv2
+from scipy.misc import *
+from scipy.ndimage.morphology import *
 
 class preprocessingImage(object):
-	def __init__(self, image_nd_array):
-		self.image_nd_array = image_nd_array
 
-	def trimBlackBoxes(self, image_array):
-		bg = Image.new(image_array.mode, image_array.size, image_array.getpixel((0,0)))
-		diff = ImageChops.difference(image_array, bg)
-		diff = ImageChops.add(diff, diff, 2.0, -100)
-		bbox = diff.getbbox()
-		if bbox:
-			return image_array.crop(bbox)
-
-	def removeBlackBoxes(self, image_nd_array):
-		return [self._trimBlackBoxes(each_array) for each_array in image_nd_array]
+	def trimBlackBoxes(self, image_array, tol=0):
+		trimmed_image = []
+		image_array = np.array(map(list, zip(*image_array)))
+		size = np.shape(image_array)
+		for i in range(0,size[0]):
+			if image_array[i].any() > tol:
+				trimmed_image.append(image_array[i])
+		trimmed_image = map(list, zip(*trimmed_image))
+		return trimmed_image
 
 	def rgbToGrayscaleConversion(self, image_nd_array):
-		imagesInGrayScale = []
+		images_grayscale = []
 		for i, each_array in enumerate(image_nd_array):
 			grayscaled_image = []
-			for pixel in each_array:
-				for row in pixel:
-					grayscaled_image.append([self.weightedAverageGrayscaleConv(pixel) for pixel in row])
-			imagesInGrayScale.append(grayscaled_image)
-		return imagesInGrayScale
+			for col in each_array:
+				grayscaled_image.append([self.weightedAverageGrayscaleConv(row) for row in col])
+			images_grayscale.append(grayscaled_image)
+		return images_grayscale
 
 	def weightedAverageGrayscaleConv(self, pixel):
-		return np.floor(0.299*pixel[0] + 0.587*pixel[1] + 0.114*pixel[2])
+		return int(0.299*pixel[0] + 0.587*pixel[1] + 0.114*pixel[2])
 
 	def padImages(self, image_array):
-		imageSize = np.shape(image_array)
-		padded_matrix = np.zeros(size[0]+2,size[1]+2)
-		padded_matrix[1:size[0],1:size[1]] = image_array
+		size = np.shape(image_array)
+		padded_matrix = np.zeros((size[0]+2,size[1]+2))
+		padded_matrix[1:size[0]+1,1:size[1]+1] = image_array
 		return padded_matrix
 		
+	def trimmedToBinary(self, image_array):
+		image_array = np.array(map(list, zip(*image_array)))
+		bi_image = []
+		for col in image_array:
+			bi_image.append([1 if pix > np.median(col) else 0 for pix in col])
+		bi_image = binary_erosion(np.array(map(list, zip(*bi_image))))
+		return bi_image
+
 	def grayscaleToBinaryConversion(self, image_nd_array):
 		binary_converted_images = []
 		for image in image_nd_array:
-			size = np.shape(image)
-			im = self._padImages(image)
-			binary_image = np.zeros(size)
-			for i in range(1,size[0]+1):
-				for j in range(1,size[1]+1):
-					list_of_pixels = [im[i-1,j-1],im[i-1,j],im[i-1,j+1],im[i,j-1],im[i,j+1],im[i+1,j-1],im[i+1,j],im[i+1,j+1]]
-					binary_image[i,j] = np.median(np.sort(list_of_pixels))
-			binary_converted_images.append(binary_image)
+			im = self.trimBlackBoxes(image)
+			binary_converted_images.append(self.trimmedToBinary(im))
 		return binary_converted_images
-
-	def mainCallerProcessor(self, image_nd_array):
-		pass
 
 class selectingPointRepresentation(object):
 	def __init__(self, image_nd_array):
