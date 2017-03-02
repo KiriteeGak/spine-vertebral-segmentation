2d35ead55a47734aa1d48351b61c26386d93b725
Kiritee <kiritee.amruth@innovaccer.com>
2017-03-02 11:06:27 +0530

    junk removed

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-'''
-This code was the research presented in my masters thesis
-The main idea behind this rewriting is to not make someone build something from scratch and 
-if this is found useful and additions to make this better are always welcome
-
-Contributors : Kiritee.GAK
-
-Loading >> Preprocessing
-Finding the vertebrae and eliminating FP
-Finding orientation of vertebrae in space for predicting next missing vertebrae
-contributors, if any, can use the same casing in naming for proper flow 
-function naming in camelcasing and variable with underscore
-
-dumps:
-> N-D Array dumps for actual images/Grayscaled/Binary are present in dumps folder
-'''
-
 from utilities import *
 from preprocessing import preprocessingImage as ppi
 from preprocessing import selectingPointRepresentation as spr
@@ -22,17 +5,11 @@ from scipy import misc, ndimage
 import os, glob, cPickle
 import numpy as np
  
-def loading_images(path):
-	return [ndimage.imread(path+"/"+filename, mode="RGB") for root, dirnames, filenames in os.walk(path) for filename in filenames]
-
-def point_representation_vertebra():
-	pass
-
-def region_growing(image, point, thershold):
-	pass
-
 def main(loaded_images):
+	ppi().rgbToGrayscaleConversion(loaded_images)
+	ppi().grayscaleToBinaryConversion(loaded_images)
 	pass
 	
 if __name__ == '__main__':
 	main(loadFromPickle("binaryT1eroded"))
+	main(loadFromPickle("grayscaledT1"))
