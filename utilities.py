c7537a9af5528d03762f75ce69c2cb0386d86af9
Kiritee <kiritee.amruth@innovaccer.com>
2017-03-02 11:08:07 +0530

    moved some more global utilities

--- utilities.py
+++ utilities.py
@@ -1,4 +1,5 @@
 import cPickle
+import numpy as np
 
 def dumpAsPickle(filename, toDump):
 	with open("dumps/"+filename+".pkl", 'wb') as fid:
@@ -7,3 +8,12 @@ def dumpAsPickle(filename, toDump):
 def loadFromPickle(filename):
 	with open("dumps/"+filename+".pkl", 'r') as fid:
 		return cPickle.load(fid)
+
+def padImages(image_array):
+	size = np.shape(image_array)
+	padded_matrix = np.zeros((size[0]+2,size[1]+2))
+	padded_matrix[1:size[0]+1,1:size[1]+1] = np.array(image_array)
+	return padded_matrix
+
+def loadingImageFiles(path):
+	return [ndimage.imread(path+"/"+filename, mode="RGB") for root, dirnames, filenames in os.walk(path) for filename in filenames]
